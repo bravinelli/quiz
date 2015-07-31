4b1ccdbb54b13a1332bf8e8d488d57c5339f4aaa
Emilio Jose Delgado <emilio.jose.delgado@gmail.com>
2015-07-31 23:59:54 +0200

    Editar Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -53,16 +53,43 @@ exports.new=function(req,res){
 };
 exports.create=function(req,res){
     var quiz=models.Quiz.build(req.body.quiz);
-    //console.log (quiz);
-    var errors = quiz.validate();//ya qe el objeto errors no tiene then(
-    if (errors)
-    {
-    var i=0; var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
-    for (var prop in errors) errores[i++]={message: errors[prop]};
-    res.render('quizes/new', {quiz: quiz, errors: errores});
-  } else {
-quiz // save: guarda en DB campos pregunta y respuesta de quiz
-.save({fields: ["pregunta", "respuesta"]})
-.then( function(){ res.redirect('/quizes')}) ;
-}
+    quiz
+    .validate()
+    .then(
+      function(err){
+        if (err) {
+          res.render('quizes/new', {quiz: quiz, errors: err.errors});
+        } else {
+          quiz // save: guarda en DB campos pregunta y respuesta de quiz
+          .save({fields: ["pregunta", "respuesta"]})
+          .then( function(){ res.redirect('/quizes')})
+        }      // res.redirect: Redirección HTTP a lista de preguntas
+      }
+    );
+  };
+
+//GET /quizes/:id/edit
+exports.edit = function(req,res){
+  var quiz = req.quiz;
+      res.render('quizes/edit',{quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+  req.quiz.pregunta  = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+  req.quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+      } else {
+        req.quiz     // save: guarda campos pregunta y respuesta en DB
+        .save( {fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes');});
+      }     // Redirección HTTP a lista de preguntas (URL relativo)
+    }
+  );
 };
